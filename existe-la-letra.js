c6ba52d200319b5307efa47588009384cb33917e
Kecar2 <kendrycarvajalsegura>
2022-03-11 09:04:33 +0100

    exer

--- existe-la-letra.js
+++ existe-la-letra.js
@@ -9,40 +9,28 @@
  * 0: Si la letra está en la palabra secreta PERO en una posición diferente
  * 1: Si la letra está en la palabra secreta Y está en la posición correcta 
  */
- function rightOrder(palabraSecreta, letra, posicion) {
-    rightOrder.forEach(element => console.log(element));
-     
-     
+
+ function rightOrder(palabraSecreta, palabraIntroducida) {
+    // array para guardar resultados
+    let resultados = [];
+    // for para comprobar cada carácter del string introducido
+    for (let i = 0; i < palabraIntroducida.length; i++) {
+        // comprobamos si se encuentra el mismo carácter en la palabra secreta 
+        if (palabraIntroducida[i] == palabraSecreta[i]) {
+            resultados.push(1);
+            //comprobamos si la palabra secreta contiene el carácter sin importar la posición
+        } else if (palabraSecreta.includes(palabraIntroducida[i])) {
+            resultados.push(0);
+            //no contiene el carácter asi que lo valoramos con -1
+        } else {
+            resultados.push(-1);
+        }
+    }//devolvemos array con los datos
+    return resultados;
 }
 
-let palabraSecreta = [
-    {
-        letra: "S",
-        posicion: 0
-    },
-    {
-        letra: "A",
-        posicion: 1
-    },
-    {
-        letra: "U",
-        posicion: 2
-    },
-    {
-        letra: "N",
-        posicion: 3
-    },
-    {
-        letra: "A",
-        posicion: 4
-    }
-];
+let palabraSecreta = "SAUNA"
 
-
-rightOrder(palabraSecreta, "I", 1); // -1
-rightOrder(palabraSecreta, "S", 4); // 0
-rightOrder(palabraSecreta, "S", 0); // 1
-
-/***
- * Recorrer el array y devolver 
- */
+console.log(palabraSecreta);
+console.log("SUEÑA:", rightOrder(palabraSecreta, "SUEÑA"));
+console.log("SALTA:", rightOrder(palabraSecreta, "SALTA")); console.log("SAUNA:", rightOrder(palabraSecreta, "SAUNA"));
